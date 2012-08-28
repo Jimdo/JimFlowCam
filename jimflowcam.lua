9e10d823d7978d78695fd21ffb99045d4daca885
michi lehr <michi@jimdo.com>
2012-08-28 16:57:51 +0200

    add license and updates readme

--- jimflowcam.lua
+++ jimflowcam.lua
@@ -1,4 +1,13 @@
 --[[
+
+Copyright (C) [2012]  [Jimdo GmbH - Michael Lehr]
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 @title JimFlowCam
 @param s Seconds 1st shot
 @default s 30
